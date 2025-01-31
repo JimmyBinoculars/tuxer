1fe0d9e2d9a07a65798ccf7f4544756c438e9615
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2025-01-31 19:07:47 +0000

    Build & Run

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Assemble everything in 64-bit and make sure it returns error when compilation failed
 
+# Ensure libX11.so is in the ./lib directory
+if [ ! -f ./lib/libX11.so ]; then
+    echo "libX11.so not found in ./lib directory."
+    echo "Please ensure you have placed libX11.so in the ./lib directory."
+    exit 1
+fi
+
 # Assemble helloWorld.asm to hello.o
 nasm -f elf64 -o build/hello.o src/helloWorld.asm
 if [ $? -ne 0 ]; then
@@ -29,8 +36,8 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Link the object files to create the final executable
-ld -o build/main build/main.o build/display.o build/hello.o build/x11.o -L./lib -lX11
+# Link the object files to create the final executable using the local libX11.so
+ld -o build/main build/main.o build/display.o build/hello.o build/x11.o -L./lib -lX11 -dynamic-linker /lib64/ld-linux-x86-64.so.2
 if [ $? -ne 0 ]; then
     echo "Error linking object files"
     exit 1
